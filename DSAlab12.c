cedd5af623d0e727d99ad13ee861076bea204ea4
Siddharth TR <siddartha.24.beai@acharya.ac.in>
2026-01-18 17:19:40 +0530

    Update DSAlab12.c

--- DSAlab12.c
+++ DSAlab12.c
@@ -4,21 +4,26 @@
   Develop a Program in C that uses Hash function H: K →L as H(K)=K mod m (remainder method), 
   and implement hashing technique to map a given key K to the address space L. Resolve the collision (if any) using 
   linear probing*/
-  #include <stdio.h>
+/* 
+Given a file of employee records stored using Hash Table.
+Hash Function: H(K) = K mod m
+Collision Resolution: Linear Probing
+*/
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 5
+#define MAX 5   // Size of Hash Table
 
 struct employee
 {
     int id;
     char name[20];
 };
-typedef struct employee EMP;
 
-EMP emp[MAX];
-int ht[MAX];
+struct employee emp[MAX];
+int ht[MAX];   // Hash table to store keys
 
 int hash(int key)
 {
@@ -28,6 +33,7 @@ int hash(int key)
 void insert()
 {
     int id, index, i;
+
     printf("Enter employee id: ");
     scanf("%d", &id);
 
@@ -39,13 +45,15 @@ void insert()
         {
             ht[index] = id;
             emp[index].id = id;
+
             printf("Enter employee name: ");
             scanf("%s", emp[index].name);
             return;
         }
-        index = (index + 1) % MAX;
+        index = (index + 1) % MAX;   // Linear probing
     }
-    printf("Hash table full\n");
+
+    printf("Hash table is full\n");
 }
 
 void display()
@@ -64,18 +72,22 @@ void display()
 int main()
 {
     int ch, i;
-    for (i = 0; i < MAX; i++) ht[i] = -1;
+
+    for (i = 0; i < MAX; i++)
+        ht[i] = -1;
 
     while (1)
     {
         printf("\n1.Insert\n2.Display\n3.Exit\nEnter choice: ");
         scanf("%d", &ch);
+
         switch (ch)
         {
-        case 1: insert(); break;
-        case 2: display(); break;
-        case 3: exit(0);
-        default: printf("Invalid choice\n");
+            case 1: insert(); break;
+            case 2: display(); break;
+            case 3: exit(0);
+            default: printf("Invalid choice\n");
         }
     }
 }
+
