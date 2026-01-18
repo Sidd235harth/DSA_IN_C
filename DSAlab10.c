5cab9eefef111772da5df1752d78f855afa9602b
Siddharth TR <siddartha.24.beai@acharya.ac.in>
2026-01-18 12:36:06 +0530

    Update DSAlab10.c

--- DSAlab10.c
+++ DSAlab10.c
@@ -7,52 +7,60 @@ d. Exit*/
 #include <stdio.h>
 #include <stdlib.h>
 
-struct BST
+/* Definition of BST node */
+struct node
 {
     int data;
-    struct BST *left, *right;
+    struct node *left;
+    struct node *right;
 };
-typedef struct BST NODE;
 
 /* Create BST */
-NODE* createtree(NODE *root, int data)
+struct node* createtree(struct node *root, int data)
 {
     if (root == NULL)
     {
-        NODE *temp = (NODE*)malloc(sizeof(NODE));
+        struct node *temp = (struct node*)malloc(sizeof(struct node));
         temp->data = data;
-        temp->left = temp->right = NULL;
+        temp->left = NULL;
+        temp->right = NULL;
         return temp;
     }
+
     if (data < root->data)
         root->left = createtree(root->left, data);
     else if (data > root->data)
         root->right = createtree(root->right, data);
+
     return root;
 }
 
-/* Traversals */
-void inorder(NODE *root)
+/* Inorder Traversal */
+void inorder(struct node *root)
 {
-    if (root)
+    if (root != NULL)
     {
         inorder(root->left);
         printf("%d ", root->data);
         inorder(root->right);
     }
 }
-void preorder(NODE *root)
+
+/* Preorder Traversal */
+void preorder(struct node *root)
 {
-    if (root)
+    if (root != NULL)
     {
         printf("%d ", root->data);
         preorder(root->left);
         preorder(root->right);
     }
 }
-void postorder(NODE *root)
+
+/* Postorder Traversal */
+void postorder(struct node *root)
 {
-    if (root)
+    if (root != NULL)
     {
         postorder(root->left);
         postorder(root->right);
@@ -60,69 +68,83 @@ void postorder(NODE *root)
     }
 }
 
-/* Search */
-void search(NODE *root, int data)
+/* Search in BST */
+void search(struct node *root, int key)
 {
     if (root == NULL)
     {
         printf("Element not found\n");
         return;
     }
-    if (data == root->data)
+
+    if (key == root->data)
         printf("Element found: %d\n", root->data);
-    else if (data < root->data)
-        search(root->left, data);
+    else if (key < root->data)
+        search(root->left, key);
     else
-        search(root->right, data);
+        search(root->right, key);
 }
 
 int main()
 {
     int ch, n, i, data;
-    NODE *root = NULL;
+    struct node *root = NULL;
 
     while (1)
     {
-        printf("\n1.Create BST\n2.Inorder\n3.Preorder\n4.Postorder\n5.Search\n6.Exit");
-        printf("\nEnter choice: ");
+        printf("\n--- BST MENU ---");
+        printf("\n1. Create BST");
+        printf("\n2. Inorder Traversal");
+        printf("\n3. Preorder Traversal");
+        printf("\n4. Postorder Traversal");
+        printf("\n5. Search");
+        printf("\n6. Exit");
+        printf("\nEnter your choice: ");
         scanf("%d", &ch);
 
         switch (ch)
         {
-        case 1:
-            printf("Enter number of nodes: ");
-            scanf("%d", &n);
-            printf("Enter elements:\n");
-            for (i = 0; i < n; i++)
-            {
+            case 1:
+                printf("Enter number of nodes: ");
+                scanf("%d", &n);
+                printf("Enter elements:\n");
+                for (i = 0; i < n; i++)
+                {
+                    scanf("%d", &data);
+                    root = createtree(root, data);
+                }
+                break;
+
+            case 2:
+                printf("Inorder Traversal: ");
+                inorder(root);
+                printf("\n");
+                break;
+
+            case 3:
+                printf("Preorder Traversal: ");
+                preorder(root);
+                printf("\n");
+                break;
+
+            case 4:
+                printf("Postorder Traversal: ");
+                postorder(root);
+                printf("\n");
+                break;
+
+            case 5:
+                printf("Enter element to search: ");
                 scanf("%d", &data);
-                root = createtree(root, data);
-            }
-            break;
-        case 2:
-            printf("Inorder Traversal:\n");
-            inorder(root);
-            printf("\n");
-            break;
-        case 3:
-            printf("Preorder Traversal:\n");
-            preorder(root);
-            printf("\n");
-            break;
-        case 4:
-            printf("Postorder Traversal:\n");
-            postorder(root);
-            printf("\n");
-            break;
-        case 5:
-            printf("Enter element to search: ");
-            scanf("%d", &data);
-            search(root, data);
-            break;
-        case 6:
-            exit(0);
-        default:
-            printf("Invalid choice\n");
+                search(root, data);
+                break;
+
+            case 6:
+                exit(0);
+
+            default:
+                printf("Invalid choice\n");
         }
     }
 }
+
